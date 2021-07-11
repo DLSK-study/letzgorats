c1da7f247ce815bb1329567c7ca68a9eceed2065
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2021-07-11 20:51:26 +0900

    Create Number Card Game.py

---
+++ Greedy/Number Card Game.py
@@ -0,0 +1,19 @@
+# 내가 푼 방법
+
+import sys
+import time
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+for i in range(n):
+    row = list(map(int, input().split()))
+    current_min = min(row)
+    if i == 0:
+        max_of_entire_min = current_min    # 우선 첫번째 입력받은 행의 최솟값은 전체 행렬의 최소값 중 최대값이다.
+
+    else:
+        if current_min > max_of_entire_min:    # 현재 행에서의 최소값이 기존의 max_of_entire_min보다 크면, max_of_entire_min을 갱신시켜준다.
+            max_of_entire_min = current_min
+
+print(max_of_entire_min)
