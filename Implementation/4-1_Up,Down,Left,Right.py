1cf20d5c0175c5c9622a56d9a54a7e432d446d2d
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2021-07-18 04:26:07 +0900

    상하좌우_(O)

---
+++ Implementation/4-1_Up,Down,Left,Right.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+plan = list(input().split())
+current_X = 0  # 현재 x 위치 (열)
+current_Y = 0  # 현재 y 위치 (행) 
+for cmd in plan:
+    if(cmd == 'L'):   # L 일 때는 ( left)
+        if(current_X == 0):   # 열 위치가 0번째인지만 확인하면 된다
+            continue
+        else:
+            current_X -= 1
+    elif(cmd == 'R'):  #  R일 때는 (right)
+        if(current_X == N-1):   # 열 위치가 N-1 번째인지만 확인하면 된다.
+            continue
+        else:
+            current_X += 1
+    elif(cmd == 'D'):   # D일 때는 (down)
+        if(current_Y == N-1):    # 행 위치가 N-1 번째인지만 확인하면 된다.
+            continue
+        else:
+            current_Y += 1
+    elif(cmd == 'U'):    #  U 일때는  (up)
+        if(current_Y == 0):   # 행 위치가 0번째인지만 확인하면 된다.
+            continue
+        else:
+            current_Y -= 1
+
+print(current_Y+1, current_X+1)
