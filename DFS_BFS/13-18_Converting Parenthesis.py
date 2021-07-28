782ff2d5261d7a39c8f7e5d4b6426105aa735694
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2021-07-29 01:28:59 +0900

    괄호 변환_(X)

--- DFS_BFS/13-18_Converting Parenthesis.py
+++ DFS_BFS/13-18_Converting Parenthesis.py
@@ -70,3 +70,52 @@ def solution(p):
         elif(answer[0]==")"):
             real_answer = ''
             return dfs(p[cnt-1:],left_length,real_answer)+answer
+        
+ ###########################################################################################################################################################
+# 답지 풀이
+def balanced_index(p):
+    left_count = 0 # 왼쪽 괄호 개수
+    for i in range(len(p)):
+        if(p[i]=="("):
+            left_count+=1
+        else:
+            left_count -=1
+        if left_count == 0:
+            return i
+            
+# 올바른 괄호 문자열인지 판단
+def check_proper(p):
+    left_count = 0 # 왼쪽 괄호 개수
+    for i in p:
+        if i=='(':
+            left_count+=1
+        else:
+            if left_count == 0: # 쌍이 맞지 않는 경우에 False 반환
+                return False
+            left_count-=1
+    return True
+
+def solution(p):
+    answer =''
+    if p =='':
+        return answer
+    index = balanced_index(p)
+    u = p[:index+1]
+    v = p[index+1:]
+    
+    # 올바른 괄호 문자열이면
+    if check_proper(u):
+        answer = u + solution(v)
+    # 올바른 괄호 문자열이 아니라면
+    else:
+        answer='('
+        answer+=solution(v)
+        answer+=')'
+        u = list(u[1:-1]) # 첫 번째와 마지막 문자 제거
+        for i in range(len(u)):
+            if(u[i]=='('):
+                u[i]=')'
+            else:
+                u[i]='('
+        answer+="".join(u)
+    return answer
