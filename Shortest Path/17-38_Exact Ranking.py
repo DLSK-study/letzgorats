d4f8541d0b7c7d1b9fe1cff178c0ded65f6584cf
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2021-08-20 09:00:44 +0900

    정확한 순위_(△)

---
+++ Shortest Path/17-38_Exact Ranking.py
@@ -0,0 +1,33 @@
+# n이 작아서 플로이드로 바로 접근
+# 풀긴 풀었는데, 최단 경로문제인 것을 알고 풀었기 때문에 다시 풀어보기
+
+import sys
+input = sys.stdin.readline
+
+INF = int(1e9)
+n , compare = map(int,input().split())
+graph = [[INF] * (n+1) for _ in range(n+1)]
+for i in range(n+1):
+    graph[i][i] = 0
+
+for i in range(compare):
+    a,b = map(int,input().split())
+    graph[a][b] = 1
+
+for k in range(1,n+1):
+    for a in range(n+1):
+        for b in range(n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
+
+possible = 0
+for i in range(1,n+1):
+    count = 0
+    for j in range(1,n+1):
+        if(graph[i][j] == INF or graph[i][j] == INF):
+            continue
+        else:    # 해당 노드에 오는 루트든 가는 루트든 다 존재하면 
+            count+=1
+    if count == (n-1):   # count가 자기자신 노드를 향한 루트를 제외한 횟수 n-1 이면 +1
+        possible +=1
+
+print(possible)
