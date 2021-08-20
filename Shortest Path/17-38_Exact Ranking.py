6bc2c114babd19cbdb6c2735000251a00e89e438
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2021-08-20 15:44:22 +0900

    정확한 순서_(X)

--- Shortest Path/17-38_Exact Ranking.py
+++ Shortest Path/17-38_Exact Ranking.py
@@ -30,4 +30,43 @@ for i in range(1,n+1):
     if count == (n-1):   # count가 자기자신 노드를 향한 루트를 제외한 횟수 n-1 이면 +1
         possible +=1
 
+print(possible) 
+'''
+오류
+'''
+# 고친 코드
+import sys
+input = sys.stdin.readline
+
+INF = int(1e9)
+n , compare = map(int,input().split())
+graph = [[INF] * (n+1) for _ in range(n+1)]
+for i in range(1,n+1):
+    graph[i][i] = 0
+
+for i in range(compare):
+    a,b = map(int,input().split())
+    graph[a][b] = 1
+
+for k in range(1,n+1):
+    for a in range(1,n+1):
+        for b in range(1,n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
+
+for a in range(1,n+1):
+    for b in range(1,n+1):
+        print(graph[a][b],end=" ")
+    print()
+
+possible = 0
+for i in range(1,n+1):
+    count = 0
+    for j in range(1,n+1):
+        if(graph[i][j]==INF and graph[j][i]==INF):
+            continue
+        else:    # 해당 노드에 오는 루트든 가는 루트든 다 존재하면 
+            count+=1
+    if count == n:   # count가 자기자신 노드를 향한 루트를 제외한 횟수 n-1 이면 +1
+        possible +=1
+
 print(possible)
